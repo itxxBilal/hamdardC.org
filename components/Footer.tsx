cc0f23aa9ed05ddb88a48d327892b8d5429753dc
Bilal <g.bilal034@gmail.com>
2025-11-29 13:50:35 +0500

    nextjs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
   }
 
   return (
-    <footer className="bg-gradient-to-br from-[#0A400C] to-[#819067] text-white">
+    <footer className="bg-linear-to-br from-[#0A400C] to-[#819067] text-white">
       {/* Main Footer */}
       <div className="max-w-7xl mx-auto px-4 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
