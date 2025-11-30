5c57d8f5908f6ddc67d5ba00ecf56d0c5dece177
Bilal <g.bilal034@gmail.com>
2025-11-30 15:40:58 +0500

    nextjs

--- app/about/team/layout.tsx
+++ app/about/team/layout.tsx
@@ -30,8 +30,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/og-images/og-ourteam.png",
-        width: 1200,
-        height: 630,
         alt: pageTitle,
       },
     ],
